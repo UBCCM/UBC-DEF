4ca5033deec0b566e9a12c8d98facfc0fbc5ef03
James <james.otoole@ubc.ca>
2017-11-07 16:39:37 -0800

    Update Gulp Tasks and output
    ADD editorconfig
    UPDATE - code formatting (to reflect editorconfig settings)
    UPDATE - sass-lint rules
    UPDATE - linted all SCSS
    UPDATE - moved mapped css to dev directory
    UPDATE - renamed 'component' task to 'minify', removed old minify task
    UPDATE - renamed styles-minimal to styles-critical

--- gulpfile.js
+++ gulpfile.js
@@ -10,81 +10,82 @@ var sasslint = require('gulp-sass-lint');
 var sassdoc = require('sassdoc');
 
 var paths = {
-    src: {
-        sass: 'src/sass/',
-        css: 'src/css/'
-    },
-    dist: {
-        css: 'dist/css/'
-    }
+	src: {
+		sass: 'src/sass/',
+		css: 'src/css/'
+	},
+	dist: {
+		css: 'dist/css/'
+	}
 };
 
 /***************************************
- * 
+ *
  *	Default CLF SASS compiling
  *  Run command "gulp" to start
  *
  ***************************************/
 
 // full list of rules at: https://github.com/sasstools/sass-lint/tree/develop/docs/rules
-gulp.task('lint', function() {
-    return gulp.src(paths.src.sass + '**/*.s+(a|c)ss')
-        .pipe(sasslint({
-            options: {
-                formatter: 'stylish',
-                'merge-default-rules': true,
-            },
-            files: {ignore: paths.src.sass + 'vendor/*.s+(a|c)ss'},
-            configFile: 'config/.sass-lint.yml'
-        }))
-        .pipe(sasslint.format())
-        .pipe(sasslint.failOnError())
+gulp.task('lint', function () {
+	return gulp.src(paths.src.sass + '**/*.s+(a|c)ss')
+		.pipe(sasslint({
+			options: {
+				formatter: 'stylish',
+				'merge-default-rules': true,
+			},
+			files: {
+				ignore: paths.src.sass + 'vendor/*.s+(a|c)ss'
+			},
+			configFile: 'config/.sass-lint.yml'
+		}))
+		.pipe(sasslint.format())
+		.pipe(sasslint.failOnError())
 });
 
 gulp.task('sassdoc', function () {
-    return gulp.src(paths.src.sass + '**/*.s+(a|c)ss')
-    .pipe(sassdoc());
+	return gulp.src(paths.src.sass + '**/*.s+(a|c)ss')
+		.pipe(sassdoc());
 });
 
-gulp.task('styles', function() {
-    return gulp.src(paths.src.sass + '*.s+(a|c)ss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(rename({
-            suffix: ".dev",
-            extname: ".css"
-        }))
-        //.pipe(rename('ubcclf.css'))
-        .pipe(autoprefixer({
-            browsers: ['last 6 versions']
-        }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(paths.dist.css))
+gulp.task('styles', function () {
+	return gulp.src(paths.src.sass + '**/*.s+(a|c)ss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(rename({
+			dirname: "dev",
+			suffix: ".dev",
+			extname: ".css"
+		}))
+		//.pipe(rename('ubcclf.css'))
+		.pipe(autoprefixer({
+			browsers: ['last 6 versions']
+		}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(paths.dist.css))
 });
 
-gulp.task('components', function() {
-    return gulp.src(paths.src.sass + '**/*.s+(a|c)ss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 6 versions']
-        }))
-        .pipe(nano({ discardComments: { removeAll: true }, zindex: false }))
-        .pipe(rename({
-            suffix: ".min",
-            extname: ".css"
-        }))
-        .pipe(gulp.dest(paths.dist.css))
+gulp.task('minify', ['styles'], function () {
+	return gulp.src(paths.src.sass + '**/*.s+(a|c)ss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer({
+		browsers: ['last 6 versions']
+	}))
+	.pipe(nano({
+		discardComments: {
+			removeAll: true
+		},
+		zindex: false
+	}))
+	.pipe(rename({
+		suffix: ".min",
+		extname: ".css"
+	}))
+	.pipe(gulp.dest(paths.dist.css))
 });
 
-gulp.task('minify', ['styles'], function() {
-    return gulp.src(paths.dist.css + '/ubcclf.css')
-        .pipe(nano({ discardComments: { removeAll: true }, zindex: false }))
-        .pipe(rename('ubcclf.min.css'))
-        .pipe(gulp.dest(paths.dist.css))
+gulp.task('watch', ['styles', 'minify'], function () {
+	gulp.watch(paths.src.sass + '/**/*.s+(a|c)ss', ['styles', 'minify']);
 });
 
-gulp.task('watch', ['styles', 'components', 'minify'], function() {
-    gulp.watch(paths.src.sass + '/**/*.s+(a|c)ss', ['styles', 'components', 'minify']);
-});
-
-gulp.task('default', ['styles', 'components', 'minify', 'watch'], function() {});
+gulp.task('default', ['styles', 'minify', 'watch'], function () {});
