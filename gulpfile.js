0611354cd7eafe52c5fe946be0ee47b869dadaa8
O'Toole, James <james.otoole@ubc.ca>
2018-12-11 15:45:29 -0800

    Update gulp to 4.x and all gulp plugins to latest version

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,32 @@
-'use strict';
+const { series, parallel, src, dest } = require('gulp');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const nano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const sasslint = require('gulp-sass-lint');
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var nano = require('gulp-cssnano');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var sasslint = require('gulp-sass-lint');
-
-var paths = {
-	src: {
-		sass: 'src/sass/',
-		css: 'src/css/'
-	},
-	dist: {
-		css: 'dist/css/'
-	}
-};
-
-/***************************************
- *
- *	Default CLF SASS compiling
- *  Run command "gulp" to start
- *
- ***************************************/
-
-// full list of rules at: https://github.com/sasstools/sass-lint/tree/develop/docs/rules
-gulp.task('lint', function () {
-	return gulp.src(paths.src.sass + '**/*.s+(a|c)ss')
+function lint() {
+	return src('src/sass/**/*.s+(a|c)ss')
 		.pipe(sasslint({
 			options: {
 				formatter: 'stylish',
 				'merge-default-rules': true,
 			},
 			files: {
-				ignore: paths.src.sass + 'vendor/*.s+(a|c)ss'
+				ignore: 'src/sass/vendor/*.s+(a|c)ss'
 			},
 			configFile: 'config/.sass-lint.yml'
 		}))
 		.pipe(sasslint.format())
 		.pipe(sasslint.failOnError())
-});
+}
 
-gulp.task('sassdoc', function () {
-	return gulp.src(paths.src.sass + '**/*.s+(a|c)ss')
-		.pipe(sassdoc());
-});
-
-gulp.task('styles', function () {
-	return gulp.src(paths.src.sass + '**/*.s+(a|c)ss')
-		.pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(rename({
-			dirname: "dev",
-			suffix: ".dev",
-			extname: ".css"
-		}))
-		//.pipe(rename('ubcclf.css'))
-		.pipe(autoprefixer({
-			browsers: ['last 6 versions']
-		}))
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(paths.dist.css))
-});
-
-gulp.task('minify', ['styles'], function () {
-	return gulp.src(paths.src.sass + '**/*.s+(a|c)ss')
+function minify() {
+	return src('src/sass/**/*.s+(a|c)ss')
 	.pipe(sass().on('error', sass.logError))
 	.pipe(autoprefixer({
-		browsers: ['last 6 versions']
+		browsers: ['last 4 versions']
 	}))
 	.pipe(nano({
 		discardComments: {
@@ -80,11 +38,32 @@ gulp.task('minify', ['styles'], function () {
 		suffix: ".min",
 		extname: ".css"
 	}))
-	.pipe(gulp.dest(paths.dist.css))
-});
+	.pipe(dest('dist/css/'))
+}
 
-gulp.task('watch', ['styles', 'minify'], function () {
-	gulp.watch(paths.src.sass + '/**/*.s+(a|c)ss', ['styles', 'minify']);
-});
+function javascript(cb) {
+  // body omitted
+  cb();
+}
+function css() {
+	return src('src/sass/**/*.s+(a|c)ss')
+	.pipe(sourcemaps.init())
+	.pipe(sass().on('error', sass.logError))
+	.pipe(rename({
+		dirname: "dev",
+		suffix: ".dev",
+		extname: ".css"
+	}))
+	//.pipe(rename('ubcclf.css'))
+	.pipe(autoprefixer({
+		browsers: ['last 6 versions']
+	}))
+	.pipe(sourcemaps.write('./'))
+	.pipe(dest('dist/css/'))
+}
 
-gulp.task('default', ['styles', 'minify', 'watch'], function () {});
+exports.lint = series(lint);
+exports.css = series(css);
+exports.minify = series(minify);
+exports.build = parallel(javascript, css);
+exports.default = series(javascript, lint);
