5e3e9f324487021c1df976ed465a88d313f230b1
O'Toole, James <james.otoole@ubc.ca>
2019-04-17 15:29:17 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,17 @@
-'use strict';
-
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var nano = require('gulp-cssnano');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var sasslint = require('gulp-sass-lint');
-var sassdoc = require('sassdoc');
+var gulp            = require('gulp');
+var sass            = require('gulp-sass');
+var rename          = require('gulp-rename');
+var nano            = require('gulp-cssnano');
+var sourcemaps      = require('gulp-sourcemaps');
+var autoprefixer    = require('gulp-autoprefixer');
 
 var paths = {
-    src: {
-        sass: 'src/sass/',
-        css: 'src/css/'
-    },
+	src: {
+		sass:	'src/sass/',
+		css: 	'src/css/'
+	},
     dist: {
-        css: 'dist/css/'
+        css:    'dist/css/'
     }
 };
 
@@ -26,36 +22,16 @@ var paths = {
  *
  ***************************************/
 
-// full list of rules at: https://github.com/sasstools/sass-lint/tree/develop/docs/rules
-gulp.task('lint', function() {
-    return gulp.src(paths.src.sass + '**/*.s+(a|c)ss')
-        .pipe(sasslint({
-            options: {
-                formatter: 'stylish',
-                'merge-default-rules': true,
-            },
-            files: {ignore: paths.src.sass + 'vendor/*.scss'},
-            configFile: 'config/.sass-lint.yml'
-        }))
-        .pipe(sasslint.format())
-        .pipe(sasslint.failOnError())
-});
-
-gulp.task('sassdoc', function () {
-    return gulp.src(paths.src.sass + '**/*.scss')
-    .pipe(sassdoc());
-});
-
 gulp.task('styles', function() {
-    return gulp.src(paths.src.sass + 'MAIN.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(rename('ubcclf.css'))
-        .pipe(autoprefixer({
-            browsers: ['last 6 versions']
-        }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(paths.dist.css))
+	return gulp.src(paths.src.sass + 'MAIN.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(rename('ubcclf.css'))
+		.pipe(autoprefixer({
+			browsers: ['last 6 versions']
+		}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(paths.dist.css))
 });
 
 gulp.task('minify', function() {
